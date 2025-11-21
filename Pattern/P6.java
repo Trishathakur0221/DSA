c6ab0c6508f175e3571e63277d2738ba34cd345d
Trishathakur0221 <trishathakur0221@gmail.com>
2025-11-21 12:04:06 +0530

    first

---
+++ Pattern/P6.java
@@ -0,0 +1,15 @@
+package Pattern;
+//print the right angle triangle at right hand side
+public class P6 {
+    public static void main(String[] args) {
+        for(int i=1;i<=6;i++){
+            for(int j=6;j>=i;j--){
+                System.out.print("  ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
