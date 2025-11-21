492b4078f01a49e6593a3c5283ceaedce7b77aad
Trishathakur0221 <trishathakur0221@gmail.com>
2025-11-21 22:36:37 +0530

    first

---
+++ Pattern/P13.java
@@ -0,0 +1,15 @@
+package Pattern;
+// print the pyramid using number
+public class P13 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=5;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print( k + " ");
+            }
+            System.out.println();
+        }
+    }
+}
