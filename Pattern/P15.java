0280f2ea370133e50db11d5ef54274c223fc5cc1
Trishathakur0221 <trishathakur0221@gmail.com>
2025-11-23 00:28:04 +0530

    first

---
+++ Pattern/P15.java
@@ -0,0 +1,15 @@
+package Pattern;
+
+public class P15 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            char ch='A';
+            for(int j=1;j<=i;j++){
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+    
+}
---
+++ Pattern/P16.java
@@ -0,0 +1,15 @@
+package Pattern;
+
+public class P16 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            char ch='A';
+            for(int j=5;j>=i;j--){
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+    
+}
