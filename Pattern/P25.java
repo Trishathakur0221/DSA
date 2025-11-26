45f9fb7dce75203ba70fc98211cc3224c5c5e9d9
Trishathakur0221 <trishathakur0221@gmail.com>
2025-11-26 09:32:26 +0530

    first

---
+++ Pattern/P25.java
@@ -0,0 +1,27 @@
+package Pattern;
+// print G using stars
+/*
+    * * * *
+    *      
+    *   * * * *
+    *     *   *
+    * * * *   *
+
+
+
+*/
+public class P25 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(i==1|| i==5|| j==1 || (i==3 && j>=3) || (j==5 && i>=3)|| (i==4 && j==4)){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
