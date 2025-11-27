cc9645839181d43a99e31f29a2400bfec3c91170
Trishathakur0221 <trishathakur0221@gmail.com>
2025-11-27 14:51:51 +0530

    first

---
+++ Pattern/P35.java
@@ -0,0 +1,18 @@
+package Pattern;
+// print U using star pattern
+public class P35 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(j==1 && i<5 || j==5 && i<5 || i==5 && j>1 && j<5){
+                // if(j==1 && i>1 || j==5 && i<5 || i==5 && j>1 && j<5){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
