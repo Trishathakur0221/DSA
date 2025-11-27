1f145f39979fb818aa5f0b06f74a38d360162217
Trishathakur0221 <trishathakur0221@gmail.com>
2025-11-27 14:36:11 +0530

    first

---
+++ Pattern/P31.java
@@ -0,0 +1,17 @@
+package Pattern;
+// print N using stars
+public class P31 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(j==1||j==5||(i==j)){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
