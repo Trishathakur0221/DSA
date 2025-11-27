795ecb4aaf85f77b0a583a3c4ee09977ea3a975d
Trishathakur0221 <trishathakur0221@gmail.com>
2025-11-27 14:42:47 +0530

    first

---
+++ Pattern/P33.java
@@ -0,0 +1,24 @@
+package Pattern;
+// print P using stars
+/*
+ * * * 
+ *     *
+ * * * 
+ * 
+ * 
+*/
+public class P33 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(j==1|| (i==1 && j<4)|| (i==3 && j<4)|| (j==4 && i>1 && i<3)){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
