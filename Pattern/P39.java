1d75ae056c30267ca9a5bbb686d826bb45b50b5d
Trishathakur0221 <trishathakur0221@gmail.com>
2025-11-29 23:26:10 +0530

    first

---
+++ Pattern/P39.java
@@ -0,0 +1,30 @@
+package Pattern;
+//inverted triangle using stars
+public class P39 {
+    public static void main(String[] args) {
+       for(int i=1;i<=5;i++) {
+        for(int j=2;j<=i;j++){
+            System.out.print(" ");
+        }
+        for(int k=5;k>=i;k--){
+            System.out.print("*");
+        }
+        for(int l=4;l>=i;l--){
+            System.out.print("*");
+        }
+        System.out.println();
+       }
+       for(int i=1;i<=5;i++){
+        for(int j=4;j>=i;j--){
+            System.out.print(" ");
+        }
+        for(int k=1;k<=i;k++){
+            System.out.print("*");
+        }
+        for(int l=2;l<=i;l++){
+            System.out.print("*");
+        }
+        System.out.println();
+       }
+    }
+}
