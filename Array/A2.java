47190da124d5cc0abe9e248a2f6f6082dc3208fd
Trishathakur0221 <trishathakur0221@gmail.com>
2025-12-11 22:51:58 +0530

    first

---
+++ Array/A2.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class A2 {
+   public static void main(String[] args) {
+    int[] marks=new int [5];
+    marks[0]=92;
+    marks[1]=98;
+    marks[2]=82;
+    marks[3]=88;
+    marks[4]=79;
+    // System.out.println(marks[4]);
+    for(int i=0;i<marks.length;i++){
+       System.out.println(marks[i]);
+    }
+   } 
+}
