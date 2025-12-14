68cf849d3088132c48dd4374bfc0071cbc5c7aaf
Trishathakur0221 <trishathakur0221@gmail.com>
2025-12-14 08:10:42 +0530

    first

---
+++ Array/A15.java
@@ -0,0 +1,16 @@
+package Array;
+//search the element in an array
+public class A15 {
+    public static void main(String[] args) {
+        int arr[]={1,3,2,6,4,8};
+        int key=4;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==key){
+                System.out.println("found at index: " + arr[i]);
+            }
+            else{
+                System.out.println("not found at index: " + arr[i]);
+            }
+        }
+    }
+}
