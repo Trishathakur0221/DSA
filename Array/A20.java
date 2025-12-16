1e781e5d8b4360954c20d9a3362416a49a82a106
Trishathakur0221 <trishathakur0221@gmail.com>
2025-12-16 14:19:33 +0530

    first

---
+++ Array/A20.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class A20 {
+    public static void main(String[] args) {
+         int arr[]={1,2,3,4,5,6,7,8};
+        int n=arr.length;
+        //storing the element
+        int temp=arr[n-1];  //temp=8
+        //shifting the element
+        for(int i=n-1;i>0;i--){
+            arr[i]=arr[i-1];
+        }
+        //place the stored element
+        arr[0]=temp;
+        //print the array
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
