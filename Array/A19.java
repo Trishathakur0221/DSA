2d6c7557c7faad026fee7736ecbc8b6c67ae3851
Trishathakur0221 <trishathakur0221@gmail.com>
2025-12-16 14:18:46 +0530

    first

--- Array/A19.java
+++ Array/A19.java
@@ -4,17 +4,24 @@ public class A19 {
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5,6,7,8};
         int n=arr.length;
-        //storing the element
-        int temp=arr[n-1];  //temp=8
-        //shifting the element
-        for(int i=n-1;i>0;i--){
-            arr[i]=arr[i-1];
+        int d=1;
+        // reverse entire array
+        reverse(arr, 0, n - 1);
+        // reverse first d elements
+        reverse(arr, 0, d - 1);
+        // reverse remaining elements
+        reverse(arr, d, n - 1);
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
-        //place the stored element
-        arr[0]=temp;
-        //print the array
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]+" ");
+    }
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
     }
 }
