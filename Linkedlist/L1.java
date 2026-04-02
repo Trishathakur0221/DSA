6489e3b0e9546f2cae820f020525396fb4d30fef
Trishathakur0221 <trishathakur0221@gmail.com>
2026-04-02 13:56:46 +0530

    hello

---
+++ Linkedlist/L1.java
@@ -0,0 +1,17 @@
+//wap to print the elements of linked list
+package Linkedlist;
+import java.util.*;
+import java.util.LinkedList;
+public class L1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+
+        System.out.println("Elements in the linked list:" + list);
+        
+    }
+}
