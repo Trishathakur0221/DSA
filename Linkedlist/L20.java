14e9afb17ffaa731078675ad5d69b17f2a48bcdf
Trishathakur0221 <trishathakur0221@gmail.com>
2026-04-08 23:38:30 +0530

    hii

---
+++ Linkedlist/L20.java
@@ -0,0 +1,37 @@
+// find the middle element of the linked list
+package Linkedlist;
+class node {
+    int data;
+    node next;
+
+    node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+public class L20 {
+    public static void main(String[] args) {
+       node head = new node(10);
+        head.next = new node(20);
+        head.next.next = new node(30);
+        head.next.next.next = new node(40);
+        head.next.next.next.next = new node(50);
+        head.next.next.next.next.next=new node(60);
+        
+        //find the middle element
+        node slow = head;
+        node fast = head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        System.out.println("the middle element of the linked list is: " + slow.data);
+
+        //traversal
+        node p=head;
+        while( p!=null){
+            System.out.println(p.data);
+            p=p.next;
+        }
+    }
+}
