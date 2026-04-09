f7cdc07a11d129d5b71f1afae6ad8808c9ec15a4
Trishathakur0221 <trishathakur0221@gmail.com>
2026-04-09 17:28:17 +0530

    hii

---
+++ Linkedlist/L22.java
@@ -0,0 +1,40 @@
+//insert a node at the beginning of the doubly linked list
+package Linkedlist;
+
+class node {
+    int data;
+    node next;
+    node prev;
+
+    node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+public class L22 {
+    public static void main(String[] args) {
+
+        node head = new node(10);
+        head.next = new node(20);
+        head.next.next = new node(30);
+        head.next.next.next = new node(40);
+
+        // insert at beginning
+        node temp = new node(50);
+
+        temp.next = head;
+        temp.prev = null;
+        head.prev = temp;
+        head = temp;
+
+        System.out.println("Elements in the linked list:");
+
+        node t = head;
+        while (t != null) {
+            System.out.print(t.data + " ");
+            t = t.next;
+        }
+    }
+}
---
+++ Linkedlist/L23.java
@@ -0,0 +1,40 @@
+//insert a node at the end of doubly linked list
+package Linkedlist;
+
+class node {
+    int data;
+    node next;
+    node prev;
+
+    node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+public class L23 {
+    public static void main(String[] args) {
+        node head = new node(10);
+        head.next = new node(20);
+        head.next.next = new node(30);
+        head.next.next.next = new node(40);
+
+        // insert a new node at the end
+        node p=new node(50);
+        node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=p;
+        p.prev=temp;
+        p.next=null;
+
+        //traversal
+        node t=head;
+        while(t!=null){
+            System.out.println(t.data);
+            t=t.next;
+        }
+    }
+}
